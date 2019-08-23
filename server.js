55eb751f4fb56d090262f8d9cd8833522613d4f8
casekercan <casekercan@gmail.com>
2019-08-23 15:35:01 -0700

    working minus speaker

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-const RSSParser = require('rss-parser')
+var express = require("express");
+var path = require("path");
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/dist'));
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+var app = express();
+var PORT = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+// =============================================================
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port);
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
